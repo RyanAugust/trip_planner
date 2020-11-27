8a5604a23698fd6c838484921c33ff5c2a2d6e95
RyanAugust <rad19206@yahoo.com>
2020-11-27 09:20:49 -0800

    updated module name

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 from datetime import datetime, timedelta
 print('Beginning Test\n-----------------')
 print('Attempting to import module...')
-import trip_timer
+import trip_planner
 print('---passed---')
 
 #### Segment Class
 print('Attempting to set segment details...')
-first = trip_timer.segment(segment_name='first',segment_start=datetime.now(), segment_time=timedelta(hours=2))
+first = trip_planner.segment(segment_name='first',segment_start=datetime.now(), segment_time=timedelta(hours=2))
 print('---passed---')
 
 print('Attempting to solve segment...')
@@ -19,12 +19,12 @@ print('---passed---')
 
 #### Trip Timer Class
 print('Attempting to establish trip (4 segments)...')
-baldy = trip_timer.trip('Baldy')
+baldy = trip_planner.trip('Baldy')
 baldy.add_segment(segment_name='drive to baldy', 
 				  segment_time=timedelta(hours=1, minutes=30))
 baldy.add_segment(segment_name='baldy ascent', 
 				  segment_time=timedelta(hours=2, minutes=40),
-				  segment_end=datetime(year=2020, month=11, day=26, hour=6, minute=35))
+				  segment_end=datetime(year=2020, month=11, day=26, hour=7, minute=1))
 baldy.add_segment(segment_name='baldy descent', 
 				  segment_time=timedelta(hours=1, minutes=50))
 baldy.add_segment(segment_name='drive back from baldy', 
